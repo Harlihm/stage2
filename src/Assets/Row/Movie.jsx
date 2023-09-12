e6ed4fad8bc21aced37079d02d1fcf40d5b91c80
Harlihm <107759287+Harlihm@users.noreply.github.com>
2023-09-12 16:26:54 +0100

    Update Movie.jsx

--- src/Assets/Row/Movie.jsx
+++ src/Assets/Row/Movie.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+/*
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/";
 import { Link } from 'react-router-dom';
 import imdb from '../../Images/imdb.png';
@@ -27,3 +27,43 @@ const Movie = ({ title, poster_path, vote_average,id, release_date }) => {
 }
 
 export default Movie
+*/
+
+import React from 'react';
+import './Row.css';
+import Movie from './Movie';
+
+const Row = ({ title, movies }) => {
+  console.log(movies);
+
+  // Define a variable to store the sliced movie groups
+  const movieGroups = [];
+
+  // Loop through the movies and slice them into groups of 10
+  for (let i = 0; i < movies.length; i += 10) {
+    const movieGroup = movies.slice(i, i + 10);
+    movieGroups.push(movieGroup);
+  }
+
+  return (
+    <div className="Featured">
+      <div className="item">
+        <h2>{title}</h2>
+        <p>See more</p>
+      </div>
+
+      {/* Map through the movieGroups array instead of the movies array */}
+      <div className="row__posters" data-testid="movie-card">
+        {movieGroups.map((group, index) => (
+          <div key={index} className="group">
+            {group.map((movie) => (
+              <Movie key={movie.id} {...movie} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
