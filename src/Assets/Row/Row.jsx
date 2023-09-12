c7947cf7183e5f3c2e9599b64fca3b481e9c5848
Harlihm <107759287+Harlihm@users.noreply.github.com>
2023-09-12 16:33:08 +0100

    Update Row.jsx

--- src/Assets/Row/Row.jsx
+++ src/Assets/Row/Row.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+/*
 import './Row.css'
 import Movie from "./Movie";
 
@@ -27,3 +28,43 @@ console.log(movies)
 }
 
 export default Row
+*/
+
+import React from 'react';
+import './Row.css';
+import Movie from './Movie';
+
+const Row = ({ title, movies }) => {
+  console.log(movies);
+
+  // Define a variable to store the sliced movie groups
+  const movieGroups = [];
+
+  // Loop through the movies and slice them into groups of 10
+  for (let i = 0; i < movies.length; i += 10) {
+    const movieGroup = movies.slice(i, i + 10);
+    movieGroups.push(movieGroup);
+  }
+
+  return (
+    <div className="Featured">
+      <div className="item">
+        <h2>{title}</h2>
+        <p>See more</p>
+      </div>
+
+      {/* Map through the movieGroups array instead of the movies array */}
+      <div className="row__posters" data-testid="movie-card">
+        {movieGroups.map((group, index) => (
+          <div key={index} className="group">
+            {group.map((movie) => (
+              <Movie key={movie.id} {...movie} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
