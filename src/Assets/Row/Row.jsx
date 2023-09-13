3b0135ac1b4e7023bc52613786a42de6db99770a
Harlihm <107759287+Harlihm@users.noreply.github.com>
2023-09-13 06:33:04 -0700

    Add files via upload

--- src/Assets/Row/Row.jsx
+++ src/Assets/Row/Row.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
-import './Row.css'
-import Movie from "./Movie";
-
-
+import './Row.css';
+import Movie from './Movie';
 
 const Row = ({ title, movies }) => {
-console.log(movies)
+  console.log(movies);
+
+  const first10Movies = movies.slice(0, 10);
+
   return (
     <div className="Featured">
       <div className="item">
@@ -14,16 +15,13 @@ console.log(movies)
       </div>
 
       <div className="row__posters" data-testid="movie-card">
-        {movies.length > 0 &&
-          movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              {...movie}
-            />
+        {first10Movies.length > 0 &&
+          first10Movies.map((movie) => (
+            <Movie key={movie.id} {...movie} />
           ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
